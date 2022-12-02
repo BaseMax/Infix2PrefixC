cc9551ebeecb579322b02abfb0e4129a585ab221
Max Base <maxbasecode@gmail.com>
2022-12-02 22:20:15 +0100

    create c program, start

---
+++ Infix2Prefix.c
@@ -0,0 +1,22 @@
+#include <stido.h>
+#include <stdlib.h>
+
+char* Infix2Prefix(char* infix)
+{
+    int len = strlen(infix);
+    char* prefix = (char*)malloc(sizeof(char) * (len + 1));
+    prefix[len] = '\0';
+    
+    return prefix;
+}
+
+int main(int argc, char** argv)
+{
+    char* infix = "1+2*3";
+    char* prefix = Infix2Prefix(infix);
+
+    printf("Infix:  %s\n", infix);
+    printf("Prefix: %s\n", prefix);
+
+    return 0;
+}
